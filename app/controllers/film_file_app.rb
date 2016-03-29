5ef422fd1222fb5b248152f75d0e4d6ddc86cb9a
Ashwin Rao <theonlyrao@gmail.com>
2016-03-28 20:42:59 -0600

    working after class

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,5 +1,16 @@
 class FilmFile < Sinatra::Base
+
+  get "/" do
+   redirect "/films"
+  end
+
   get '/films' do
+    @films = Film.all
     erb :films_index
+  end
+
+  get "/genres" do
+    @genres = Genre.all
+    erb :genres_index
   end
 end
