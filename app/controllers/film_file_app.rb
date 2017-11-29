ffd85a29f7f1c556ff62f6f2bb07b28a295deeac
Ilana Corson <ilana@turing.io>
2017-11-28 18:49:39 -0700

    update with model testing

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -1,14 +1,6 @@
 class FilmFile < Sinatra::Base
-<<<<<<< HEAD
-
-=======
->>>>>>> 1710_live
   get '/films' do
     @films = Film.all
     erb :"films/index"
   end
-<<<<<<< HEAD
-
-=======
->>>>>>> 1710_live
 end
