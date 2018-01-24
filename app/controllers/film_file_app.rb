96a4eef7505e84470db9cb86344cde9df3217d18
Ali Schlereth <aschlere@sbcglobal.net>
2018-01-24 10:58:49 -0700

    Revert to using instance variable

--- app/controllers/film_file_app.rb
+++ app/controllers/film_file_app.rb
@@ -2,11 +2,10 @@ class FilmFile < Sinatra::Base
 
   get '/films' do
     @films = Film.all
-    erb :"films/index"
+    erb(:"films/index")
   end
 
   get '/films/:id' do
-    require "pry"; binding.pry
     @film = Film.find(params[:id])
     erb :"films/show"
   end
